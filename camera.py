1a424a6556c24149a8145a1da4efe74783fe42da
WRO <wro@gmail.com>
2025-04-10 12:17:07 +0100

    Refactor camera color detection: update HSV thresholds for red and add white color range

--- camera.py
+++ camera.py
@@ -13,7 +13,6 @@ os.environ['LIBCAMERA_LOG_LEVELS'] = '4'
 
 class Camera:
 	img = None
-	hsv_img = None
 	visuals_img = None
 
 	frame = 0
@@ -31,6 +30,10 @@ class Camera:
 			"angle": 0,
 			"distance": 0,
 			"center": (0, 0),
+			"mask": None,
+		},
+		"white": {
+			"detected": False,
 			"mask": None,
 		}
 	}
@@ -85,7 +88,12 @@ class Camera:
 
 			img = Camera.img.copy()
 
-			for color in Camera.colors:
+			# Draw Floor over img
+			white = Camera.colors["white"].copy()
+			if white["mask"] is not None:
+				img = cv2.addWeighted(img, 1, white["mask"], 1, 0)
+
+			for color in ["green", "red"]:
 				if Camera.colors[color]["detected"]:
 					# Draw a point at the center of the detected traffic sign with it's color
 					cv2.circle(img, Camera.colors[color]["center"], 5, (255, 0, 0), -1)
@@ -104,7 +112,7 @@ class Camera:
 
 		# Return the image as a byte array
 		return jpeg.tobytes()
-	
+
 	@staticmethod
 	def get_traffic_signs():
 		last_frame = 0
@@ -118,76 +126,19 @@ class Camera:
 				sleep(1/240)
 				continue
 
-			Camera.hsv_img = cv2.cvtColor(Camera.img.copy(), cv2.COLOR_BGR2HSV)
+			hsv_img = cv2.cvtColor(Camera.img.copy(), cv2.COLOR_BGR2HSV)
 
 			for color in Camera.colors:
-				Camera.colors[color]["detected"], Camera.colors[color]["mask"], Camera.colors[color]["distance"], Camera.colors[color]["angle"], Camera.colors[color]["center"]  = Camera.process_traffic_sign(Config.config["camera"]["colors"][color])
+				Camera.colors[color]["mask"] = Camera.get_hsv_mask_from_config(hsv_img, Config.config["camera"]["colors"][color])
+
+
+				if color in ["green", "red"]:
+					Camera.colors[color]["detected"], Camera.colors[color]["distance"], Camera.colors[color]["angle"], Camera.colors[color]["center"]  = Camera.process_traffic_sign(Camera.colors[color]["mask"])
 
 			#closest_color = Camera.colors["green"] if Camera.colors["green"]["distance"] < Camera.colors["red"]["distance"] else Camera.colors["red"]
 
 	@staticmethod
-	def process_traffic_sign(config):
-		hsv =  Camera.hsv_img.copy()
-		im = Camera.img
-
-		h_min = config["lower"][0]
-		h_max = config["upper"][0]
-		s_min = config["lower"][1]
-		s_max = config["upper"][1]
-		v_min = config["lower"][2]
-		v_max = config["upper"][2]
-
-		if h_min > h_max:
-			lower1 = (0, 0, 0)
-			upper1 = (h_max, 255, 255)
-			mask1 = cv2.inRange(hsv, lower1, upper1)
-
-			lower2 = (h_min, 0, 0)
-			upper2 = (179, 255, 255)
-			mask2 = cv2.inRange(hsv, lower2, upper2)
-
-			mask = cv2.bitwise_or(mask1, mask2)
-		else:
-			lower = (h_min, 0, 0)
-			upper = (h_max, 255, 255)
-
-			mask = cv2.inRange(hsv, lower, upper)
-
-		if s_min > s_max:
-			lower1 = (0, 0, 0)
-			upper1 = (179, s_max, 255)
-			mask1 = cv2.inRange(hsv, lower1, upper1)
-
-			lower2 = (0, s_min, 0)
-			upper2 = (179, 255, 255)
-			mask2 = cv2.inRange(hsv, lower2, upper2)
-
-			mask3 = cv2.bitwise_or(mask1, mask2)
-			mask = cv2.bitwise_and(mask, mask3)
-		else:
-			lower = (0, s_min, 0)
-			upper = (179, s_max, 255)
-			mask1 = cv2.inRange(hsv, lower, upper)
-			mask = cv2.bitwise_and(mask, mask1)
-
-		if v_min > v_max:
-			lower1 = (0, 0, 0)
-			upper1 = (179, 255, v_max)
-			mask1 = cv2.inRange(hsv, lower1, upper1)
-
-			lower2 = (0, 0, v_min)
-			upper2 = (179, 255, 255)
-			mask2 = cv2.inRange(hsv, lower2, upper2)
-
-			mask3 = cv2.bitwise_or(mask1, mask2)
-			mask = cv2.bitwise_and(mask, mask3)
-		else:
-			lower = (0, 0, v_min)
-			upper = (179, 255, v_max)
-			mask1 = cv2.inRange(hsv, lower, upper)
-			mask = cv2.bitwise_and(mask, mask1)
-
-
+	def process_traffic_sign(mask):
 		biggest_contour = None
 
 		# Find biggest countour
@@ -196,29 +147,63 @@ class Camera:
 			biggest_contour = max(contours, key=cv2.contourArea)
 
 		if biggest_contour is None:
-			return None, mask, 9999999, None, None
+			return None, 9999999, None, None
 
 		x, y, w, h = cv2.boundingRect(biggest_contour)
 		center_x = x + w // 2
 		center_y = y + h
 
-		if center_x == im.shape[1] // 2:
+		if center_x == mask.shape[1] // 2:
 			center_x += 1
 
-		if center_y == im.shape[0]:
+		if center_y == mask.shape[0]:
 			center_y += 1
 
-		A = (im.shape[1] // 2, im.shape[0])
-		B = (center_x, im.shape[0])
+		A = (mask.shape[1] // 2, mask.shape[0])
+		B = (center_x, mask.shape[0])
 		C = (center_x, center_y)
 
 		line_angle, _, _ = printAngle(A, B, C)
 
 		line_angle = 90 - line_angle
 
-		if (center_x - im.shape[1] // 2) < 0:
+		if (center_x - mask.shape[1] // 2) < 0:
 			line_angle = -line_angle
 
-		line_distance = math.sqrt((center_x - im.shape[1] // 2) ** 2 + (center_y - im.shape[0]) ** 2)
+		line_distance = math.sqrt((center_x - mask.shape[1] // 2) ** 2 + (center_y - mask.shape[0]) ** 2)
 
-		return True, mask, line_distance, line_angle, (center_x, center_y)
+		return True, line_distance, line_angle, (center_x, center_y)
+	
+	@staticmethod
+	def get_hsv_mask_from_config(hsv, config):
+		h_min, s_min, v_min = config["lower"]
+		h_max, s_max, v_max = config["upper"]
+
+		# HUE mask
+		if h_min > h_max:
+			mask1 = cv2.inRange(hsv, (0, 0, 0), (h_max, 255, 255))
+			mask2 = cv2.inRange(hsv, (h_min, 0, 0), (179, 255, 255))
+			h_mask = cv2.bitwise_or(mask1, mask2)
+		else:
+			h_mask = cv2.inRange(hsv, (h_min, 0, 0), (h_max, 255, 255))
+
+		# SATURATION mask
+		if s_min > s_max:
+			mask1 = cv2.inRange(hsv, (0, 0, 0), (179, s_max, 255))
+			mask2 = cv2.inRange(hsv, (0, s_min, 0), (179, 255, 255))
+			s_mask = cv2.bitwise_or(mask1, mask2)
+		else:
+			s_mask = cv2.inRange(hsv, (0, s_min, 0), (179, s_max, 255))
+
+		# VALUE mask
+		if v_min > v_max:
+			mask1 = cv2.inRange(hsv, (0, 0, 0), (179, 255, v_max))
+			mask2 = cv2.inRange(hsv, (0, 0, v_min), (179, 255, 255))
+			v_mask = cv2.bitwise_or(mask1, mask2)
+		else:
+			v_mask = cv2.inRange(hsv, (0, 0, v_min), (179, 255, v_max))
+
+		# Combine all
+		mask = cv2.bitwise_and(h_mask, s_mask)
+		mask = cv2.bitwise_and(mask, v_mask)
+		return mask
